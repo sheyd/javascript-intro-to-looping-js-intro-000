e7d883b4f73682ff0f822d6c9cc8802ea9abd374
Sena Heydari <sheydari@dosomething.org>
2020-05-09 03:16:25 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,13 +1,21 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
+<<<<<<< HEAD
     if (i === 1) {
       array.push(`I am ${i} strange loop.`);
     } else {
       array.push(`I am ${i} strange loops.`);
+=======
+    if (i = 1) {
+      array.push("I am `${i}` strange loop.");
+    } else {
+      array.push("I am `${i}` strange loops.");
+>>>>>>> f692fe2e506b9539f331ef2c408157f465248d69
     }
   }
   return array;
 }
+<<<<<<< HEAD
 
 function whileLoop (number) {
   while (number > 0) {
@@ -28,3 +36,5 @@ function doWhileLoop (integer) {
     console.log("I run once regardless.");
   } while (incrementVariable() <= integer);
 }
+=======
+>>>>>>> f692fe2e506b9539f331ef2c408157f465248d69
